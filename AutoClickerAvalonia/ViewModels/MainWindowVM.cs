5b73d6caff716746207b9b8657769c8fb4cf659e
rafaelmuricy <rafael_muricy@hotmail.com>
2026-03-27 00:33:48 -0300

    implementing the front end using avalonia

---
+++ AutoClickerAvalonia/ViewModels/MainWindowVM.cs
@@ -0,0 +1,15 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using WinAPIHandler;
+
+namespace AutoClickerAvalonia.ViewModels;
+
+public partial class MainWindowVM : ObservableObject
+{
+    [ObservableProperty]
+    string clock;
+
+    [ObservableProperty]
+    private List<Click> clicks = new();
+}
---
+++ AutoClickerAvalonia/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace AutoClickerAvalonia.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
